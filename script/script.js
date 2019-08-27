e442421f1174adf6e19e270d4940453df3d492e1
robertom123 <netpig00@gmail.com>
2019-08-27 03:05:48 -0700

    positioned images

--- script/script.js
+++ script/script.js
@@ -22,17 +22,40 @@ const API_KEY = 'a36d708508cabf272699a4cebbc2d896';
 const url = 'https://api.themoviedb.org/3/movie/384018?api_key=' + API_KEY;
 const url2 = 'https://api.themoviedb.org/3/discover/movie?api_key=a36d708508cabf272699a4cebbc2d896&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 const url3 = 'https://api.themoviedb.org/3/discover/movie?api_key=a36d708508cabf272699a4cebbc2d896&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&append_to_response=videos';
+const url4 = 'https://api.themoviedb.org/3/discover/movie?api_key=a36d708508cabf272699a4cebbc2d896&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 
 
-
-fetch(url2)
+fetch(url4)
     .then(res => {
         if (res.status !== 200){
             console.log('wrong api');
         }
         else{
-            res.json()
-            .then(data => {
+            res.json().then(data => {
+                console.log(data);
+                data.results.forEach(item => {
+                    let row_content = document.getElementById('row-content');
+                    console.log(item.title);
+                    let poster_path = item.poster_path;
+                    let img_path = 'http://image.tmdb.org/t/p/w185/' + poster_path;
+                   
+                    console.log(img_path);
+                    console.log(poster_path);
+
+                    let li = document.createElement('li');
+                    li.className = 'row-imgs';
+                    let img = document.createElement('img');
+                    img.setAttribute('src', img_path);
+                    //li.textContent = item.title;
+                    li.appendChild(img);
+                    row_content.appendChild(li);
+                    console.log(li);
+
+                    
+                })
+            })
+
+                /*
                 let div = document.getElementById('test');
                 let ul = document.getElementById('discover');
                 let li = document.createElement('li');
@@ -49,6 +72,18 @@ fetch(url2)
                 li.appendChild(span);
                 ul.appendChild(li);
                 console.log(data);
-            })
+                */
+           
         }
+    })
+
+    const left = document.getElementById('test-btn');
+    const right = document.getElementById('test-btn2');
+
+    right.addEventListener('click', () => {
+        document.getElementById('row-content').scrollLeft +=800;
+    });
+
+    left.addEventListener('click', ()=> {
+        document.getElementById('row-content').scrollLeft -= 700;
     })
